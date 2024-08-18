3a3d804458da8f16b43b84448527e0162f8fa53e
Bohdan Fedorv <bogdanfedorov@gmail.com>
2024-08-18 06:38:47 +0300

    refactor app module to new clientProxy function util

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,26 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { Services } from './services/types';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { clientProxy } from './utils/clientProxy';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AuthController],
-  providers: [
-    {
-      provide: Services.Auth,
-      useFactory: (configService: ConfigService) => {
-        return ClientProxyFactory.create({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get('AUTH_SERVICE_HOST'),
-            port: Number(configService.get('AUTH_SERVICE_PORT')),
-          },
-        });
-      },
-      inject: [ConfigService],
-    },
-  ],
+  providers: [clientProxy(Services.Auth)],
 })
 export class AppModule {}
